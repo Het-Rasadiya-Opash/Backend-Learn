d6c7843dec9ca20ff79f8d565f142de2cc4a54c1
Het <hra.opash@gmail.com>
2026-02-12 15:06:29 +0530

    fix: enhance playlist controller with error handling and complete CRUD operations

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,53 +1,157 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Playlist} from "../models/playlist.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
-
+import mongoose, { isValidObjectId } from "mongoose";
+import { Playlist } from "../models/playlist.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { Video } from "../models/video.model.js";
 
 const createPlaylist = asyncHandler(async (req, res) => {
-    const {name, description} = req.body
-
-    //TODO: create playlist
-})
+  const { name, description } = req.body;
+  if (!name || !description) {
+    throw new ApiError(400, "Name and description are required");
+  }
+  const playlist = await Playlist.create({
+    name,
+    description,
+    owner: req.user._id,
+  });
+  res
+    .status(201)
+    .json(new ApiResponse(201, "Playlist created successfully", playlist));
+});
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
-    const {userId} = req.params
-    //TODO: get user playlists
-})
+  const { userId } = req.params;
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(400, "Invalid user ID");
+  }
+  const playlists = await Playlist.find({ owner: userId }).populate("videos");
+
+  if (playlists.length === 0) {
+    return res
+      .status(200)
+      .json(new ApiResponse(200, [], "No playlists found for this user"));
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, playlists, "User playlists fetched successfully")
+    );
+});
 
 const getPlaylistById = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
-    //TODO: get playlist by id
-})
+  const { playlistId } = req.params;
+  if (!isValidObjectId(playlistId)) {
+    throw new ApiError(400, "Invalid playlist ID");
+  }
+  const playlist = await Playlist.findById(playlistId).populate("videos");
+  if (!playlist) {
+    throw new ApiError(404, "Playlist not found");
+  }
+  res
+    .status(200)
+    .json(new ApiResponse(200, playlist, "Playlist fetched successfully"));
+});
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
-    const {playlistId, videoId} = req.params
-})
+  const { playlistId, videoId } = req.params;
+  if (!isValidObjectId(playlistId) || !isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid playlist ID or video ID");
+  }
+  const playlist = await Playlist.findById(playlistId);
+  if (!playlist) {
+    throw new ApiError(404, "Playlist not found");
+  }
+  if (playlist.owner.toString() !== req.user._id.toString()) {
+    throw new ApiError(
+      403,
+      "You are not authorized to add video to this playlist"
+    );
+  }
+  const video = await Video.findById(videoId);
+  if (!video) {
+    throw new ApiError(404, "Video not found");
+  }
+  playlist.videos.push(videoId);
+  await playlist.save();
+  res
+    .status(200)
+    .json(
+      new ApiResponse(200, playlist, "Video added to playlist successfully")
+    );
+});
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
-    const {playlistId, videoId} = req.params
-    // TODO: remove video from playlist
-
-})
+  const { playlistId, videoId } = req.params;
+  if (!isValidObjectId(playlistId) || !isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid playlist ID or video ID");
+  }
+  const playlist = await Playlist.findById(playlistId);
+  if (!playlist) {
+    throw new ApiError(404, "Playlist not found");
+  }
+  if (playlist.owner.toString() !== req.user._id.toString()) {
+    throw new ApiError(
+      403,
+      "You are not authorized to remove video from this playlist"
+    );
+  }
+  const videoIndex = playlist.videos.indexOf(videoId);
+  if (videoIndex === -1) {
+    throw new ApiError(404, "Video not found in playlist");
+  }
+  playlist.videos.splice(videoIndex, 1);
+  await playlist.save();
+  res
+    .status(200)
+    .json(
+      new ApiResponse(200, playlist, "Video removed from playlist successfully")
+    );
+});
 
 const deletePlaylist = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
-    // TODO: delete playlist
-})
+  const { playlistId } = req.params;
+  if (!isValidObjectId(playlistId)) {
+    throw new ApiError(400, "Invalid playlist ID");
+  }
+  const playlist = await Playlist.findByIdAndDelete(playlistId);
+  if (!playlist) {
+    throw new ApiError(404, "Playlist not found");
+  }
+  res
+    .status(200)
+    .json(new ApiResponse(200, null, "Playlist deleted successfully"));
+});
 
 const updatePlaylist = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
-    const {name, description} = req.body
-    //TODO: update playlist
-})
+  const { playlistId } = req.params;
+  const { name, description } = req.body;
+
+  if (!isValidObjectId(playlistId)) {
+    throw new ApiError(400, "Invalid playlist ID");
+  }
+  const playlist = await Playlist.findById(playlistId);
+  if (!playlist) {
+    throw new ApiError(404, "Playlist not found");
+  }
+  if (playlist.owner.toString() !== req.user._id.toString()) {
+    throw new ApiError(403, "You are not authorized to update this playlist");
+  }
+  if (name) playlist.name = name;
+  if (description) playlist.description = description;
+  await playlist.save();
+  res
+    .status(200)
+    .json(new ApiResponse(200, playlist, "Playlist updated successfully"));
+});
 
 export {
-    createPlaylist,
-    getUserPlaylists,
-    getPlaylistById,
-    addVideoToPlaylist,
-    removeVideoFromPlaylist,
-    deletePlaylist,
-    updatePlaylist
-}
+  createPlaylist,
+  getUserPlaylists,
+  getPlaylistById,
+  addVideoToPlaylist,
+  removeVideoFromPlaylist,
+  deletePlaylist,
+  updatePlaylist,
+};
