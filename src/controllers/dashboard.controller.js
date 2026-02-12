3f849b2417731d876886e2e36ee5d5ccd00c2723
Het <hra.opash@gmail.com>
2026-02-12 18:07:48 +0530

    dashboard api

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,20 +1,54 @@
-import mongoose from "mongoose"
-import {Video} from "../models/video.model.js"
-import {Subscription} from "../models/subscription.model.js"
-import {Like} from "../models/like.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose from "mongoose";
+import { Video } from "../models/video.model.js";
+import { Subscription } from "../models/subscription.model.js";
+import { Like } from "../models/like.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
-    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
-})
+  const { channelId } = req.params;
+
+  const totalVideos = await Video.countDocuments({ owner: channelId });
+
+  const totalSubscribers = await Subscription.countDocuments({
+    channel: channelId,
+  });
+
+  const totalLikes = await Like.countDocuments({
+    video: { $in: await Video.find({ owner: channelId }).select("_id") },
+  });
+
+  const totalViews = await Video.aggregate([
+    { $match: { owner: new mongoose.Types.ObjectId(channelId) } },
+    { $group: { _id: null, totalViews: { $sum: "$views" } } },
+  ]);
+
+  const stats = {
+    totalVideos,
+    totalSubscribers,
+    totalLikes,
+    totalViews: totalViews[0] ? totalViews[0].totalViews : 0,
+  };
+
+  res
+    .status(200)
+    .json(new ApiResponse(true, "Channel stats fetched successfully", stats));
+});
 
 const getChannelVideos = asyncHandler(async (req, res) => {
-    // TODO: Get all the videos uploaded by the channel
-})
+  const { channelId } = req.params;
+  const videos = await Video.find({
+    owner: new mongoose.Types.ObjectId(channelId),
+  }).sort({
+    createdAt: -1,
+  });
+  if (!videos) {
+    throw new ApiError(404, "No videos found for this channel");
+  }
+  res
+    .status(200)
+    .json(new ApiResponse(true, "Channel videos fetched successfully", videos));
+});
 
-export {
-    getChannelStats, 
-    getChannelVideos
-    }
+export { getChannelStats, getChannelVideos };
