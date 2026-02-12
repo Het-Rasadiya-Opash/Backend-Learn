a9ba7e300f5142789df905b7244d9f30bccad765
Het <hra.opash@gmail.com>
2026-02-12 11:27:59 +0530

    fix: correct typo in pipeline definition for getWatchHistory

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -364,7 +364,7 @@ const getWatchHistory = asyncHandler(async (req, res) => {
               localField: "owner",
               foreignField: "_id",
               as: "owner",
-              pipline: [
+              pipeline: [
                 {
                   $project: {
                     fullName: 1,
